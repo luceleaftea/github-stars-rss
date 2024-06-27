8a73e136e3589539a01f3ebd434a0c6e68a1952e
Tyler Luce <tjluce@mac.com>
2024-06-27 00:25:58 -0400

    Remove comment

--- src/open-tofu/main.tf
+++ src/open-tofu/main.tf
@@ -55,8 +55,6 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "opml_lambda" {
-    # If the file is not in the current working directory you will need to include a
-    # path.module in the filename.
     filename      = "lambda_function_payload.zip"
     function_name = "generate_opml_file"
     role          = aws_iam_role.iam_for_lambda.arn
